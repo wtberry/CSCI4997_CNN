037a790735742c04610f73fe51fd6b83e373de67
Wataru Takahashi <universe3523@gmail.com>
2018-04-15 20:11:43 -1000

    working CNN

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ CNN implementation with pytorch, with MNIST dataset
 '''
 
 from __future__ import print_function
+from model import CNN
 import argparse
 import torch
 import torch.nn as nn
@@ -13,30 +14,69 @@ from torch.autograd import Variable
 
 ##### Setting up hyper parameters
 batch_size = 100
-lr = 0.03
+lr = 0.1
 mom = 0.5
 
 ##### Import Dataset #####
 
 # Load dataset and create dataloaders
 
+train_dataset = datasets.MNIST(root = './data/', train=True,
+                                transform=transforms.ToTensor(), download=True)
+
+test_dataset = datasets.MNIST(root = './data/', train=False,
+                                transform=transforms.ToTensor())
+
+# Data loader
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                            batch_size=batch_size,
+                                            shuffle=True)
+
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
+                                            batch_size=batch_size,
+                                            shuffle=False)
 ##### Define model and Optimizer #####
 model = CNN()
 optimizer = optim.SGD(model.parameters(), lr=lr, momentum=mom)
+criterion = nn.CrossEntropyLoss()
 
 ##### Training #####
 def train(epoch):
     model.train() ##what for??
     for batch_idx, (data, target) in enumerate(train_loader):
         data, target = Variable(data), Variable(target)
+        #print(target.shape)
         optimizer.zero_grad()
         output = model(data)
-        loss = F.nll_loss(output, target)
+        #print("output:", output.shape)
+        loss = criterion(output, target)
         loss.backward()
         optimizer.step()
-            if batch_idx % 10 == 0:
-                print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                    epoch, batch_idx * len(data), len(train_loader.dataset),
-                    100. * batch_idx / len(train_loader), loss.data[0]))
+        if batch_idx % 10 == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+            epoch, batch_idx * len(data), len(train_loader.dataset),
+            100. * batch_idx / len(train_loader), loss.data[0]))
+                    
+def test():
+    model.eval() ##??
+    test_loss = 0
+    correct = 0
+    for data, target in test_loader:
+        data, target = Variable(data, volatile=True), Variable(target)
+        output = model(data)
+        # sum up batch loss
+        #print('shape of target:', target.shape)
+        test_loss += criterion(output, target).data[0]
+        # get the index of teh max
+        pred = output.data.max(1, keepdim=True)[1]
+        correct += pred.eq(target.data.view_as(pred)).cpu().sum()
 
-
+    test_loss /= len(test_loader.dataset)
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+    
+for epoch in range(1, 10):
+    train(epoch)
+    test()
+                    
--- model.py
+++ model.py
@@ -14,27 +14,27 @@ class CNN(nn.Module):
         super(CNN, self).__init__()
 
         # Defining layers
-        self.conv1 = nn.Conv2d(1, 10, kernel_sizes=5)
-        self.conv2 = nn.Conv2d(10, 20, kernel_sizes=5)
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
         self.mp = nn.MaxPool2d(2)
-        self.fc = nn.Linear(100, 10)
+        self.fc = nn.Linear(320, 10)
 
     def forward(self, x):
-        print('size of x:', x.shape)
+        #print('size of x:', x.shape)
         input_size = x.size(0)
         xc1 = self.conv1(x)
-        print('size of xc1:', xc1.shape)
+        #print('size of xc1:', xc1.shape)
         xmp1 = self.mp(xc1)
-        print('size of xmp1:', xmp1.shape)
+        #print('size of xmp1:', xmp1.shape)
         x_relu1 = F.relu(xmp1)
 
         xc2 = self.conv2(x_relu1)
-        print('size of xc2:', xc2.shape)
+        #print('size of xc2:', xc2.shape)
         xmp2 = self.mp(xc2)
-        print('size of xmp2:', xmp2.shape)
+        #print('size of xmp2:', xmp2.shape)
         x_relu2 = F.relu(xmp2)
+        x_relu2 = x_relu2.view(input_size, -1) # Flatten the vecgtor
         xfc = self.fc(x_relu2)
-        print('size of fc:', xfc.shape)
+        #print('size of fc:', xfc.shape)
         # softmax returns probability of each candidates 
-        return F.log_softmax(x)
-
+        return xfc
